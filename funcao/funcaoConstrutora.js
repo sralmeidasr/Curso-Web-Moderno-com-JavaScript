eaae77c8ab909969a97078a60455e8f97f104f05
Sr&almeida <alan@sralmeida.com.br>
2019-05-28 17:09:30 -0300

    Adicionando uma aula sobre funções construtoras

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,28 @@
+// Por convenção, as funções construtoras tem a primeira letra em maiúsculo. 
+
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // método publico
+    this.acelerar = function() {
+        if(velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // método publico
+    this.getVelocidadeAtual = function() {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
