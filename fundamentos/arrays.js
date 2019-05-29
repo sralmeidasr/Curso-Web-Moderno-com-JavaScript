2531be1b7e2fa7a1385d0a79e37bbca595679361
Sr&almeida <alan@sralmeida.com.br>
2019-05-29 16:38:14 -0300

    Adicionando arquivo 'exercDeQulquerNaturesa.js' para prática de exercícios e atualizando o arquivo 'arrays.js' com mais exemplos

--- fundamentos/arrays.js
+++ fundamentos/arrays.js
@@ -17,3 +17,29 @@ console.log(valores)
 console.log(valores.pop()) // essa função retira o último elemento do array
 delete valores[0] // outra maneira de excluir um elemento da array
 console.log(valores)
+
+/********************************************************************************************************************/
+
+const frutas = ['Maçã', 'Banana']
+console.log(frutas[0], frutas[3])
+console.log(frutas[4])
+
+frutas[4] = 'Uva'
+console.log(frutas)
+console.log(frutas.length) /* retona quantos elementos eu tenho dentro do arrays */
+
+frutas.unshift('Morango') /* essa função serve para adicinar mais coisas dentro do array, porém o 'unshift('dado')' adiciona 
+sempre ao início do array. */
+frutas.push({id: 3}, false, null, 'teste') /* essa função serve para adicinar mais coisas dentro do array, porém o 'push()' 
+adiciona sempre ao final do array, sendo que nesse caso foram adicionados tipos de dados diferentes, o que não é adequado mas 
+erve para mostrar a flexibilidade de JavaScript. */  
+ 
+console.log(frutas)
+
+console.log(frutas.shift()) /* essa função retira o primeiro elemento do array e mostra só o elemento retirado */
+console.log(frutas.pop()) /* essa função retira o último elemento do array e mostra só o elemento retirado */
+
+delete frutas[0] /* outra maneira de excluir um elemento da array, porém diferente das funções 'shift' e 'pop', ao usarmos 
+'delete array[0]' ele mostra a mensagem '<1 empty item>' correspondente ao indice do array. */ 
+
+console.log(frutas)
