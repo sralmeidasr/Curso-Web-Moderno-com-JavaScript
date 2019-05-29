562806f120512c25c8ca66ef357fe06513e75a4b
Sr&almeida <alan@sralmeida.com.br>
2019-05-29 12:51:20 -0300

    Adicionando aula sobre funções auto-invocadas

---
+++ funcao/iife.js
@@ -0,0 +1,6 @@
+// IIFE - Immediatly Invoked Function Expression = 'funções auto-invocadas'.
+
+(function() {    
+    console.log('Será executando na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
